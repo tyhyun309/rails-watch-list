658676ed1507c235fd4ca1268d34bd1ec20cd162
Yin Lee <leesrrh@gmail.com>
2023-05-12 00:56:45 +0900

    done first day

---
+++ app/models/bookmark.rb
@@ -0,0 +1,12 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :movie_id, presence: true
+  validates :list_id, presence: true
+  validates :comment, presence: true
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: {
+    scope: :list_id,
+    message: 'is already in the list'
+  }
+end
