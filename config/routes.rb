d0dccac9cac785298cf237338e9dedf6b04e13a8
Yin Lee <leesrrh@gmail.com>
2023-05-12 12:18:41 +0900

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'lists#index'
+
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
