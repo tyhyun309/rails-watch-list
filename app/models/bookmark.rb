f0eb6352a98e8ec9aa3e2481453caaa20c367164
Yin Lee <leesrrh@gmail.com>
2023-07-23 19:37:57 +0900

    db

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -6,8 +6,8 @@ class Bookmark < ApplicationRecord
   validates :list_id, presence: true
   validates :comment, presence: true
   validates :comment, length: { minimum: 6 }
-  validates :movie_id, uniqueness: {
-    scope: :list_id,
+  validates :watch_movie_id, uniqueness: {
+    scope: :watch_list_id,
     message: 'is already in the list'
   }
 end
